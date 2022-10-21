28fd97a33c320b67edc6e926cde28ecd38845218
siraamer <siraamer@hotmail.com>
2022-10-21 03:42:58 +0200

    add testimonials section

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ const closeNav = () => {
 };
 
 closeNavBtn.addEventListener("click", closeNav);
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
