4afdc41bfb2f2b7b5ce212ec8116626f7eeb09d8
siraamer <siraamer@hotmail.com>
2022-10-24 04:54:34 +0200

    Finish project

--- main.js
+++ main.js
@@ -16,6 +16,21 @@ const closeNav = () => {
 
 closeNavBtn.addEventListener("click", closeNav);
 
+//close nav menu when menu is clicked
+if (window.innerWidth < 1024) {
+  document.querySelectorAll("#nav__items li a").forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      closeNav();
+    });
+  });
+}
+
+// change nav style on scroll
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
